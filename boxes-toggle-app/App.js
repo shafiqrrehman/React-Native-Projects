36e0058e73d6c3d40d2a0f29e9e3218f387bf934
shafiqurrehman <shafiq.12@live.com>
2018-03-01 12:38:00 +0500

    Detailed understanding of flexbox algorithm / app implemented

---
+++ boxes-toggle-app/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Toggle from './components/Toggle';
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  }
+
+  render() {
+
+    const {flexDirection, alignItems, justifyContent} = this.state;
+    const layoutStyle = {flexDirection, justifyContent, alignItems};
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical';
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal';
+
+    return (
+      <View style={styles.container}>
+        <Toggle 
+          label={'Primary axis (flexDirection)'} 
+          value={flexDirection} 
+          options={['row', 'column']}
+          onChange={(option) => this.setState({flexDirection: option})}
+         />
+
+         <Toggle 
+          label={primaryAxis + ' distribution (justifyContent)'} 
+          value={justifyContent} 
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
+          onChange={(option) => this.setState({justifyContent: option})}
+         />
+
+         <Toggle 
+          label={secondaryAxis + ' alignment (alignItems)'} 
+          value={alignItems} 
+          options={['flex-start', 'center', 'flex-end', 'stretch']}
+          onChange={(option) => this.setState({alignItems: option})}
+         />
+
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box} />
+          <View style={styles.box} />
+          <View style={styles.box} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+  box: {
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5
+  }
+});
